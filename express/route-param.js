0d4ff3d0e2135bee14695f1125aeb668cc5a1db1
Njun852 <njunjuntillaz@gmail.com>
2023-12-17 11:15:21 +0800

    Finished learning route and query string parameters

--- express/route-param.js
+++ express/route-param.js
@@ -14,25 +14,4 @@ app.get('/api/products/:productID', (req, res) => {
     return res.send(product)
 })
 
-app.get('/api/v1/query', (req, res) => {
-    
-    const {search, limit} = req.query
-
-    let filteredProduct = [...products]
-
-    if(search){
-        filteredProduct = filteredProduct
-        .filter(product => product.name.startsWith(search))
-    }
-    if(limit){
-        filteredProduct = filteredProduct.splice(0, Number(limit))
-    }
-    if(filteredProduct.length < 1){
-        return res.status(200)
-        .json({success: true, data: []})
-    }
-    res.status(200)
-    .json({success: true, data: filteredProduct})
-})
-
 app.listen(5000, () => console.log("listening on port 5000..."))
