aeae6cb6a9efe48d4802355382c1b85191e9d8b1
Njun852 <njunjuntillaz@gmail.com>
2024-02-26 10:21:27 +0800

    Created app

---
+++ maui/MauiApp2_Juntilla/MauiApp2_Juntilla/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp2_Juntilla
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ maui/MauiApp2_Juntilla/MauiApp2_Juntilla/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp2_Juntilla
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
