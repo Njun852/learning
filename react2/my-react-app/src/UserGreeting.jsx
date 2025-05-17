d1b4c7d138b2648d2204064055c6e3a0295dc06c
Njun852 <njunjuntillaz@gmail.com>
2025-05-17 19:55:46 +0800

    conditional rendering

---
+++ react2/my-react-app/src/UserGreeting.jsx
@@ -0,0 +1,6 @@
+export default function UserGreeting(props) {
+    const welcomeMessage = <h2 className="welcome-message">Welcome {props.username}</h2>
+    const loginPrompt = <h2 className="login-prompt">Please login to continue</h2>
+    
+    return props.isLoggedIn ? welcomeMessage : loginPrompt;
+}
