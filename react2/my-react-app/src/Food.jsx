27b3340fda89197fb85b43b3e7c54240989b9e5a
Njun852 <njunjuntillaz@gmail.com>
2025-05-17 15:32:18 +0800

    bro code tutorial

---
+++ react2/my-react-app/src/Food.jsx
@@ -0,0 +1,10 @@
+export default function Food() {
+    const food1 = "Apple";
+    const food2 = "Banana";
+    return (
+        <ul>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    )
+}
---
+++ react2/my-react-app/src/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="http://">Home</a></li>
+                    <li><a href="http://">About</a></li>
+                    <li><a href="http://">Services</a></li>
+                    <li><a href="http://">Contact</a></li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    )
+}
