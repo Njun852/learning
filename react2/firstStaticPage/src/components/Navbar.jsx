0e5a9ed307fb85f779c0364dc567e2157282f466
Njun852 <njunjuntillaz@gmail.com>
2025-05-17 11:34:31 +0800

    static page

---
+++ react2/firstStaticPage/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <header className="header">
+            <img src="./react.webp" alt="" className="header-logo"/>
+            <h2>ReactFacts</h2>
+
+        </header>
+    )
+}
